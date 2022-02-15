bc6f102ae58d7070ee4cf58d9f3fda7af513b9f5
Brett Henderson <bretth18@gmail.com>
2022-02-15 12:01:37 -0800

    added invariant and read only coverage to module manager test suite

--- tests/zoramodulemanager.test.ts
+++ tests/zoramodulemanager.test.ts
@@ -71,4 +71,156 @@ describe('Zora V3 Module Manager', () => {
   });
 
   // CONTRACT FUNCTIONS
+
+  describe('contract functions', () => {
+    let moduleManagerConfig: ZoraConfiguredAddresses;
+    const provider = new JsonRpcProvider();
+    const [mainWallet, otherWallet] = generatedWallets(provider);
+
+    beforeEach(async () => {
+      await blockchain.resetAsync();
+      moduleManagerConfig = await setupZora(mainWallet);
+    });
+
+    describe('setApprovalForModule', () => {
+      // 01
+      it('throws an erorr on read only instance', async () => {
+        const provider = new JsonRpcProvider();
+        const moduleManager = new ZoraModuleManager(
+          provider,
+          50,
+          moduleManagerConfig.moduleManager
+        );
+
+        expect(moduleManager.readOnly).toBe(true);
+
+        await expect(
+          moduleManager.setApprovalForModule(moduleManagerConfig.asksV11, true)
+        ).rejects.toThrow('ensureReadOnly: Cannot modify read-only instance');
+      });
+
+      // 02
+      it('throws an error if the module address is not valid', async () => {
+        const moduleManager = new ZoraModuleManager(
+          mainWallet,
+          50,
+          moduleManagerConfig.moduleManager
+        );
+
+        await expect(
+          moduleManager.setApprovalForModule('pee pee poo poo', true)
+        ).rejects.toThrow(
+          'Invariant failed: pee pee poo poo is not a valid address'
+        );
+      });
+    });
+
+    describe('setBatchApprovalForModules', () => {
+      // 01
+      it('throws an erorr on read only instance', async () => {
+        const provider = new JsonRpcProvider();
+        const moduleManager = new ZoraModuleManager(
+          provider,
+          50,
+          moduleManagerConfig.moduleManager
+        );
+
+        expect(moduleManager.readOnly).toBe(true);
+
+        await expect(
+          moduleManager.setBatchApprovalForModules(
+            [moduleManagerConfig.asksV11],
+            true
+          )
+        ).rejects.toThrow('ensureReadOnly: Cannot modify read-only instance');
+      });
+
+      // 02
+      it('throws an error if the module address is not valid', async () => {
+        const moduleManager = new ZoraModuleManager(
+          mainWallet,
+          50,
+          moduleManagerConfig.moduleManager
+        );
+
+        await expect(
+          moduleManager.setBatchApprovalForModules(['pee pee poo poo'], true)
+        ).rejects.toThrow(
+          'Invariant failed: pee pee poo poo is not a valid address'
+        );
+      });
+    });
+
+    describe('setApprovalForModuleBySig', () => {
+      // 01
+      it('throws an erorr on read only instance', async () => {
+        const provider = new JsonRpcProvider();
+        const moduleManager = new ZoraModuleManager(
+          provider,
+          50,
+          moduleManagerConfig.moduleManager
+        );
+
+        expect(moduleManager.readOnly).toBe(true);
+
+        await expect(
+          moduleManager.setApprovalForModuleBySig(
+            moduleManagerConfig.asksV11,
+            mainWallet.address,
+            true,
+            100,
+            '0x',
+            '0x',
+            '0x'
+          )
+        ).rejects.toThrow('ensureReadOnly: Cannot modify read-only instance');
+      });
+
+      // 02
+      it('throws an error if the module address is not valid', async () => {
+        const moduleManager = new ZoraModuleManager(
+          mainWallet,
+          50,
+          moduleManagerConfig.moduleManager
+        );
+
+        await expect(
+          moduleManager.setApprovalForModuleBySig(
+            'pee pee poo poo',
+            mainWallet.address,
+            true,
+            100,
+            '0x',
+            '0x',
+            '0x'
+          )
+        ).rejects.toThrow(
+          'Invariant failed: pee pee poo poo is not a valid address'
+        );
+      });
+
+      // 03
+      it('throws an error if the wallet address is not valid', async () => {
+        const moduleManager = new ZoraModuleManager(
+          mainWallet,
+          50,
+          moduleManagerConfig.moduleManager
+        );
+
+        await expect(
+          moduleManager.setApprovalForModuleBySig(
+            moduleManagerConfig.asksV11,
+            'pee pee poo poo',
+            true,
+            100,
+            '0x',
+            '0x',
+            '0x'
+          )
+        ).rejects.toThrow(
+          'Invariant failed: pee pee poo poo is not a valid address'
+        );
+      });
+    });
+  });
 });
