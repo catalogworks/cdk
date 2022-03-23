7da06d4f9237ca8ea28819c5907f3820fd5ff551
Brett Henderson <bretth18@gmail.com>
2022-03-22 21:01:26 -0700

    oops

--- src/reserveAuctionFindersETH.ts
+++ src/reserveAuctionFindersETH.ts
@@ -40,7 +40,7 @@ export class ReserveAuctionFindersETH {
     } else {
       // Get contract address from chainId
       const network = chainIdToNetwork(this.chainId);
-      this.contractAddress = zoraAddresses[network].reserveAuctionFindersEth;
+      this.contractAddress = zoraAddresses[network].reserveAuctionsFindersEth;
     }
 
     this.contract = ReserveAuctionFindersEth__factory.connect(
