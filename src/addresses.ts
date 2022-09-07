a344e839ad4f16ea702f0c028bbe1753bae49056
Brett Henderson <bretth18@gmail.com>
2022-09-07 12:01:56 -0700

    add reserve auction modules but they still havent included them in the package wtf

--- src/addresses.ts
+++ src/addresses.ts
@@ -37,10 +37,20 @@ export const zoraAddresses: AddressBook = {
     reserveAuctionListingEth: ZoraRinkebyAddresses.ReserveAuctionListingEth,
     reserveAuctionListingErc20: ZoraRinkebyAddresses.ReserveAuctionListingErc20,
   },
+  /// NOTE: I am hardcoding these due to inconsitent package support from Zora
   goerli: {
     moduleManager: ZoraGoerliAddresses.ZoraModuleManager,
     erc20TransferHelper: ZoraGoerliAddresses.ERC20TransferHelper,
     erc721TransferHelper: ZoraGoerliAddresses.ERC721TransferHelper,
+    /// NOTE: AsksV1.1 goerli deployment address from here (not available in package) https://github.com/richardryangarcia/zora-v3-ask-goerli
+    asks: '0x4Ae7072bbfF93F452855d32Bf90E1230Bf7deB26',
+    offers: '',
+    reserveAuctionCoreEth: '0x2506d9f5a2b0e1a2619bcce01cd3e7c289a13163',
+    reserveAuctionCoreErc20: '0x1ee71c10e7dd6c7fbdc891de4e902e041e1f5d33',
+    reserveAuctionFindersEth: '0x29a6237e646a5a189db197a48cb96fa7944a32a2',
+    reserveAuctionFindersErc20: '0x36ab5200426715a9dd414513912970cb7d659b3c',
+    reserveAuctionListingEth: '0xfcebe0788d5772df2cbcf5079815de98a4d62b09',
+    reserveAuctionListingErc20: '0x517f7721f3c3762f7048e03919761e027d900082',
   },
   mainnet: {
     moduleManager: ZoraMainnetAddresses.ZoraModuleManager,
